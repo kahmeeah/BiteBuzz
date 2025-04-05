bbbeed1c1451f1abf839b1fcffab38fb32aa3382
kahmeeah <kao8144@nyu.edu>
2025-04-04 22:14:16 -0400

    chore: pylint requested changes

--- web-app/app.py
+++ web-app/app.py
@@ -1,8 +1,10 @@
-import os 
+"""main app.py for flask"""
+# import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """renders index.html"""
     return render_template("index.html")
