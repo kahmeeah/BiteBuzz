a2070e0c55dca20a3e25633b2118fac6307116d5
ajokt123 <at4933@nyu.edu>
2025-04-09 21:09:59 -0400

    fix lint

--- machine-learning-client/ml_client.py
+++ machine-learning-client/ml_client.py
@@ -30,12 +30,13 @@ def process_unprocessed_reviews():
     for review_doc in unprocessed_reviews:
         review_text = review_doc["text"]
         _id = review_doc["_id"]
-
         sentiment_result = analyze_sentiment(review_text)
-        key_word = category.lower()
-        suggestion = generate_suggestion(review_text, sentiment_result["sentiment"],key_word)
         categories = detect_category_map(review_text)
         category = categories[0]
+        key_word = category.lower()
+        suggestion = generate_suggestion(
+            review_text, sentiment_result["sentiment"], key_word
+        )
 
         date = datetime.now().strftime("%B %d %I:%M%p")
 
