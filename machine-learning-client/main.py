b81d2ba21308d00cae41846419222fdea81bac29
nyjur1 <nbilkui@gmail.com>
2025-04-10 11:43:45 -0400

    fix lint issues

--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -2,7 +2,8 @@ from helpers.analyze_sentiment import analyze_sentiment
 from helpers.detect_category_map import detect_category_map
 from helpers.generate_suggestion import generate_suggestion
 
-def process_review (review: str):
+
+def process_review(review: str):
     sentiment_data = analyze_sentiment(review)
     sentiment = sentiment_data["sentiment"]
     categories = detect_category_map(review)
@@ -12,10 +13,10 @@ def process_review (review: str):
         for category in categories:
             suggestion = generate_suggestion(review, sentiment, category)
             if suggestion:
-                suggestions.append((category, suggestion))
-    
+                suggestions.append({"category": category, "suggestion": suggestion})
+
     return {
         "sentiment": sentiment,
         "categories": categories,
-        "suggestions": suggestions
+        "suggestions": suggestions,
     }
